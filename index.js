de72a64702580c81127e9ae532195de252445d4b
Justin <rayjustin481@gmail.com>
2024-06-21 14:02:42 +0300

    function param

---
+++ index.js
@@ -0,0 +1,9 @@
+function introduction(name) {
+    return "Hi, my name is ${name}."
+}
+function introductionwithlanguage(name, language) {
+    return "Hi, my name is [name] and i am learning to program in [language]"
+}
+function introductionwithlanguageoptional(name, language="Javascript") {
+    
+}
